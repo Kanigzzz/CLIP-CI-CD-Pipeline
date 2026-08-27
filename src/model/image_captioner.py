d0189786c0227f0e7805f9b12ab2a95760b12ea2
Kamil <chaberkamil5@gmail.com>
2026-08-27 14:25:24 +0200

    Dokonczenie struktury modelu przez budowaniem API

--- src/model/image_captioner.py
+++ src/model/image_captioner.py
@@ -1,6 +1,7 @@
 import torch
 import yaml
 from PIL import Image
+from io import BytesIO
 from pathlib import Path
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
@@ -26,11 +27,12 @@ class ImageCaptioner:
         self.processor = BlipProcessor.from_pretrained(cfg['data']['model'])
         self.model = BlipForConditionalGeneration.from_pretrained(
             cfg['data']['model'])
+        self.model.to(self.device)
         self.model.eval()
 
-    def generate_caption(self, image_path: str) -> str:
+    def generate_caption(self, images_bytes: bytes) -> str:
         try:
-            raw_img = Image.open(image_path).convert("RGB")
+            raw_img = Image.open(BytesIO(images_bytes)).convert("RGB")
             inputs = self.processor(
                 raw_img, return_tensors="pt").to(self.device)
 
