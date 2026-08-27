571fcab51d8cb739b5e2a095f94c6af20458be12
Kamil <chaberkamil5@gmail.com>
2026-08-27 12:24:22 +0200

    Dodanie modelu BLIP

---
+++ src/model/image_captioner.py
@@ -0,0 +1,31 @@
+import torch
+import yaml
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+with open("../configs/config_image_captioner.yaml", "r", encoding="utf-8") as f:
+    cfg = yaml.safe_load(f)
+
+
+class ImageCaptioner:
+    def __init__(self):
+        self.device = cfg['data']['device']
+        self.processor = BlipProcessor.from_pretrained(cfg['data']['model'])
+        self.model = BlipForConditionalGeneration.from_pretrained(
+            cfg['data']['model'])
+        self.model.eval()
+
+    def generate_caption(self, image_path: str) -> str:
+        try:
+            raw_img = Image.open(image_path).convert("RGB")
+            inputs = self.processor(
+                raw_img, return_tensors="pt").to(self.device)
+
+            with torch.no_grad():
+                out = self.model.generate(**inputs, max_new_tokens=20)
+
+            caption = self.processor.decode(out[0], skip_special_tokens=True)
+            return caption
+
+        except Exception as e:
+            return f"Error: {str(e)}"
